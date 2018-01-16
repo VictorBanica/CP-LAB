e6973ddfd047a8f4421e39305c4041aa399f4e5e
VictorBanica <32846243+VictorBanica@users.noreply.github.com>
2018-01-16 22:39:00 +0200

    Add files via upload

---
+++ lab 4 - i pb 5.py
@@ -0,0 +1,23 @@
+for i in range(2,61,2):
+    print(i);
+
+print("\n\n");
+
+i = 2;
+while i <=60:
+    print(i);
+    i += 2;
+
+print("\n\n");
+
+i = 2;
+
+print("\n\n");
+
+ok = 1;
+while ok:
+    print(i);
+    i += 2;
+    if i > 60:
+        ok = 0;
+
